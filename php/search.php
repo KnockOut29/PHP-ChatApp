c3457af79e3294c6c9471e4b55f786305f45283c
KnockOut29 <hanna.tuominen97@gmail.com>
2022-01-26 16:15:46 +0200

    correcting errors

--- php/search.php
+++ php/search.php
@@ -1,15 +1,17 @@
-<?php 
+<?php
+    session_start();
     include_once "config.php";
+
+    $outgoing_id = $_SESSION['unique_id'];
     $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
+
+    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
     $output = "";
-
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%'");
-    if(mysqli_num_rows($sql) > 0){
-        $output .= "user found";
-        include "data.php";
-
+    $query = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($query) > 0){
+        include_once "data.php";
     }else{
-        $output .= "no users found";
+        $output .= 'No user found related to your search term';
     }
-    echo $output; 
+    echo $output;
 ?>
